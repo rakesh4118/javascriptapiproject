85788897d52db81da92500dd9db84b94fad4a36a
rakesh4118 <rakii.s1>
2023-07-21 20:11:28 +0530

    GETcrud

--- booking.js
+++ booking.js
@@ -1,13 +1,19 @@
-function showNewUserOnScreen(user) {
+
+  // Initialize bookingDetails as an empty object
+  let bookingDetails = {};
+
+  function showNewUserOnScreen(user) {
     const bookingStatus = document.getElementById("bookingStatus");
-    bookingStatus.innerHTML = `
-        <h2>Latest Booking</h2>
-        <p><strong>Name:</strong> ${user.name}</p>
-        <p><strong>Email:</strong> ${user.email}</p>
-        <p><strong>Phonenumber:</strong> ${user.phonenumber}</p>
+    bookingStatus.innerHTML += `
+      <h2>Booking Successful</h2>
+      <p><strong>Name:</strong> ${user.name}</p>
+      <p><strong>Email:</strong> ${user.email}</p>
+      <p><strong>Date:</strong> ${user.phonenumber}</p>
+      <hr />
     `;
-}
-document.getElementById("bookingForm").addEventListener("submit", function (event) {
+  }
+
+  document.getElementById("bookingForm").addEventListener("submit", function (event) {
     event.preventDefault();
 
     // Get form values
@@ -15,31 +21,43 @@ document.getElementById("bookingForm").addEventListener("submit", function (even
     const email = document.getElementById("email").value;
     const phonenumber = document.getElementById("phonenumber").value;
 
-    // Create an object to hold the booking details
-    const bookingDetails = {
-        name: name,
-        email: email,
-        phonenumber: phonenumber
+    // Update the bookingDetails object
+    bookingDetails = {
+      name: name,
+      email: email,
+      phonenumber: phonenumber
     };
 
     // Save the booking details to local storage
     //localStorage.setItem(bookingDetails.email, JSON.stringify(bookingDetails));
+
+    axios
+      .post("https://crudcrud.com/api/b9542a55f7554658aab3caecb5ad7023/appointmentData", bookingDetails)
+      .then((response) => {
     
-    axios.post("https://crudcrud.com/api/0a7229e432b54427a1659e3369218e54/appointmentData",bookingDetails)
-        .then((response) => {
-            showNewUserOnScreen(response.data)
-            console.log(response)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-                //
-                    const bookingStatus = document.getElementById("bookingStatus");
-                    bookingStatus.innerHTML = `
-                        <h2>Latest Booking</h2>
-                        <p><strong>Name:</strong> ${bookingDetails.name}</p>
-                        <p><strong>Email:</strong> ${bookingDetails.email}</p>
-                        <p><strong>Phonenumber:</strong> ${bookingDetails.phonenumber}</p>
-                    `;
-    });
-   
+        showNewUserOnScreen(response.data);
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  // Function to display the user details on page load
+  window.addEventListener("DOMContentLoaded", () => {
+    axios
+      .get("https://crudcrud.com/api/b9542a55f7554658aab3caecb5ad7023/appointmentData")
+      .then((response) => {
+        console.log(response);
+        const users = response.data;
+        const bookingStatus = document.getElementById("bookingStatus");
+        bookingStatus.innerHTML = ""; // Clear the previous content
+
+        for (var i = 0; i < users.length; i++) {
+          showNewUserOnScreen(users[i]);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
